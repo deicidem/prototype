4e5fdf6d3be5692468a6712246caf94dcaa622b5
Andrey <kronosdidi@gmail.com>
2025-02-26 21:12:27 +0300

    increase tests time

--- apps/root/src/components/__tests__/AppHeader.test.ts
+++ apps/root/src/components/__tests__/AppHeader.test.ts
@@ -2,7 +2,7 @@ import { describe, expect, it } from 'vitest';
 
 describe('appHeader', () => {
     it('should render', async () => {
-        await new Promise(resolve => setTimeout(resolve, 10000));
+        await new Promise(resolve => setTimeout(resolve, 60000));
         expect(true).toBe(true);
     }, 65000);
 });
