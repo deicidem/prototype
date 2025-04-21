c27a4d9b335c692531eae5afd56f0bc8e4289214
deicidem <39920617+deicidem@users.noreply.github.com>
2025-04-21 18:28:09 +0000

    Deploying to gh-pages from @ deicidem/prototype@bf6f9052079e1621cbb1f5ed42f2ff29fa637b40 🚀

--- assets/BDashboard-BS_ikQVD.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,Q as o,R as r,S as s,V as n}from"./index-BX8OT-kx.js";import{V as _}from"./VCard-DngQ0PTx.js";const a={};function c(i,t){return r(),o(_,{class:"w-100","min-height":"200px"},{title:s(()=>t[0]||(t[0]=[n(" Дашборд команды B ")])),_:1})}const f=e(a,[["render",c]]);export{f as _};
