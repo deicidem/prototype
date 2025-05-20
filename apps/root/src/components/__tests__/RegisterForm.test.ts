9d89c441ba292a9cf877e7796990d00e6978138c
Andrey <kronosdidi@gmail.com>
2025-05-20 21:49:08 +0300

    add more tests

---
+++ apps/root/src/components/__tests__/RegisterForm.test.ts
@@ -0,0 +1,8 @@
+import { describe, expect, it } from 'vitest';
+
+describe('register-form', () => {
+    it('should render', async () => {
+        await new Promise(resolve => setTimeout(resolve, 60000));
+        expect(true).toBe(true);
+    }, 65000);
+});
